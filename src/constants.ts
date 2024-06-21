f56074312f4bde2b48849ae0b7888e5512e19596
Adolfo <allorenter@gmail.com>
2024-06-21 14:38:45 +0200

    Definición de constantes

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const MARVEL_API_BASE_URL = 'http://gateway.marvel.com/v1/public';
+
+export const MARVEL_API_PUBLIC_KEY = 'f79c0c3576b19e52b96b05b03ddade9c';
+
+export const LOCALSTORAGE_FAVORITES_CHARACTERS_KEY = 'favoritesList';
