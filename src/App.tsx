29bbeda9a7cbd4cb301dfdf9b3c38d94bb6aa2b2
Adolfo <allorenter@gmail.com>
2024-06-21 14:40:47 +0200

    Context provider para los favoritos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './router';
+import FavoritesCharactersContext from './contexts/FavoritesCharactersContext';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [favoritesList, setFavoritesList] = useState([]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <FavoritesCharactersContext.Provider value={[favoritesList, setFavoritesList]}>
+        <RouterProvider router={router} />
+      </FavoritesCharactersContext.Provider>
     </QueryClientProvider>
   );
 }
