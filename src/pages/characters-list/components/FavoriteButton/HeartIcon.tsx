8cef9da17e241cab7cc28056aed10724b2fbd17e
Adolfo <allorenter@gmail.com>
2024-06-21 19:17:05 +0200

    Fix tamaño iconos

--- src/pages/characters-list/components/FavoriteButton/HeartIcon.tsx
+++ src/pages/characters-list/components/FavoriteButton/HeartIcon.tsx
@@ -25,7 +25,7 @@ export default function HeartIcon({
   }
 
   return (
-    <svg width='24' height='22' viewBox='0 0 24 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg width='26' height='25' viewBox='0 0 26 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         className={hovered ? styles['heartIconHovered'] : styles['heartIcon']}
         fill-rule='evenodd'
