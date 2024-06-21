768410529c673b94f595c1ce5c24133995e8a05a
Adolfo <allorenter@gmail.com>
2024-06-21 14:37:01 +0200

    Añado types

---
+++ src/types/characters-api.d.ts
@@ -0,0 +1,83 @@
+export interface CharactersAPIResponse {
+  code: number;
+  status: string;
+  copyright: string;
+  attributionText: string;
+  attributionHTML: string;
+  etag: string;
+  data: Data;
+}
+
+interface Data {
+  offset: number;
+  limit: number;
+  total: number;
+  count: number;
+  results: Result[];
+}
+
+interface Result {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: Thumbnail;
+  resourceURI: string;
+  comics: Comics;
+  series: Comics;
+  stories: Stories;
+  events: Comics;
+  urls: URL[];
+}
+
+interface Comics {
+  available: number;
+  collectionURI: string;
+  items: ComicsItem[];
+  returned: number;
+}
+
+interface ComicsItem {
+  resourceURI: string;
+  name: string;
+}
+
+interface Stories {
+  available: number;
+  collectionURI: string;
+  items: StoriesItem[];
+  returned: number;
+}
+
+interface StoriesItem {
+  resourceURI: string;
+  name: string;
+  type: ItemType;
+}
+
+enum ItemType {
+  Cover = 'cover',
+  Empty = '',
+  InteriorStory = 'interiorStory',
+}
+
+interface Thumbnail {
+  path: string;
+  extension: Extension;
+}
+
+enum Extension {
+  GIF = 'gif',
+  Jpg = 'jpg',
+}
+
+interface URL {
+  type: URLType;
+  url: string;
+}
+
+enum URLType {
+  Comiclink = 'comiclink',
+  Detail = 'detail',
+  Wiki = 'wiki',
+}
---
+++ src/types/favorite-character.d.ts
@@ -0,0 +1,5 @@
+import { Character } from './character';
+
+export interface FavoriteCharacter extends Character {
+  createTime: Date;
+}
