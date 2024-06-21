bd226c45e6105af3a0d87c6cb12f8350fad73df4
Adolfo <allorenter@gmail.com>
2024-06-21 19:13:00 +0200

    Corrijo errores con types

--- src/contexts/FavoritesCharactersContext.tsx
+++ src/contexts/FavoritesCharactersContext.tsx
@@ -1,4 +1,4 @@
-import { FavoriteCharacter } from '@/pages/characters-list';
+import { FavoriteCharacter } from '@/types/favorite-character';
 import { createContext } from 'react';
 
 type SetFavoriteCharacters = React.Dispatch<React.SetStateAction<FavoriteCharacter[]>>;
