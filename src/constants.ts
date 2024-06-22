20e36da55cd7b8a79b47afcc0d68076dbba3b710
Adolfo <allorenter@gmail.com>
2024-06-22 16:39:14 +0200

    Cambio a https

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const MARVEL_API_BASE_URL = 'http://gateway.marvel.com/v1/public';
+export const MARVEL_API_BASE_URL = 'https://gateway.marvel.com/v1/public';
 
 export const MARVEL_API_PUBLIC_KEY = 'f79c0c3576b19e52b96b05b03ddade9c';
 
